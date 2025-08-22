785f61a56cd81d4643590ea807674ee3e750ee61
boggypop57 <boggy_pop@mail.ru>
2025-08-22 16:41:33 +0300

    changed after crash

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import asyncio
 import logging
-import signal
-import sys
 from os import getenv
 from typing import cast
 
@@ -13,17 +10,11 @@ from aiogram.types import Message
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ImportError:
-    pass 
-
-
 TOKEN = getenv("BOT_TOKEN")
-if TOKEN is None:
-    raise ValueError("BOT_TOKEN environment variable is not set")
+assert TOKEN is not None, "BOT_TOKEN environment variable is not set"
 
+WEB_SERVER_HOST = "0.0.0.0"
+WEB_SERVER_PORT = 8000
 WEBHOOK_HOST = "https://verysimplesaverbot-production.up.railway.app" 
 WEBHOOK_PATH = f"/webhook/ffwo2o3im23424lmvss9902lmseklfmvm23z0z0z0m"  
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
@@ -47,24 +38,29 @@ async def echo_handler(message: Message) -> None:
 
 async def on_startup(bot: Bot) -> None:
     await bot.set_webhook(WEBHOOK_URL)
+    logging.info(f"Webhook set to: {WEBHOOK_URL}")
 
 async def on_shutdown(bot: Bot) -> None:
     await bot.delete_webhook()
+    logging.info("Bot stopped. Webhook deleted.")
 
-async def main() -> None:
+def main() -> None:
     bot = Bot(token=cast(str,TOKEN), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+    
     app = web.Application()
+    
     webhook_requests_handler = SimpleRequestHandler(
         dispatcher=dp,
         bot=bot
     )
     webhook_requests_handler.register(app, path=WEBHOOK_PATH)
     setup_application(app, dp, bot=bot)
-    web.run_app(app, host="0.0.0.0", port=8000)
+    logging.info("Starting web server...")
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, lambda s, f: 
-                on_shutdown(Bot(token=cast(str,TOKEN), 
-                default=DefaultBotProperties(parse_mode=ParseMode.HTML))))
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    main()
